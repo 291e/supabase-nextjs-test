e840612b365c8691546c025ce66b0933cbad9393
291e <yko0321@naver.com>
2025-02-26 13:16:10 +0900

    Oauth2.0 Goolge 연동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
-export default function Home() {
+import { signOut } from "@/utils/supabase/actions";
+import { createClientForServer } from "@/utils/supabase/server";
+import Link from "next/link";
+
+export default async function Home() {
+  const supabase = await createClientForServer();
+
+  const session = await supabase.auth.getUser();
+
+  if (!session.data.user) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen gap-4">
+        <h1 className="text-4xl font-bold">Not Authenticated</h1>
+        <Link className="border rounded px-2.5 py-2" href="/auth">
+          Sign in
+        </Link>
+      </div>
+    );
+  }
+
+  const {
+    data: {
+      user: { user_metadata, app_metadata },
+    },
+  } = session;
+
+  const { email } = user_metadata;
+
+  const Email = email ? `${email}` : "email Not Set";
+
+  console.log(session);
   return (
-    <div className="flex flex-col items-center justify-center mx-auto py-4">
-      Home
+    <div className="flex flex-col gap-4">
+      <p className="text-xl">Email: {Email}</p>
+
+      <form action={signOut}>
+        <button className="border rounded px-2.5 py-2" type="submit">
+          Sign Out
+        </button>
+      </form>
     </div>
   );
 }
